404c3b1b88561409f0acc28fe4bf3dcecfc5f994
jeliasalvaradoa <josealvarado30@hotmail.com>
2023-02-07 02:12:18 -0400

    actualización

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -13,27 +13,27 @@ const MobileMenu = () => {
           <a href="./">All</a>
         </li>
         <li>
-          <a href="./clothes">Clothes</a>
+          <a href="#/clothes">Clothes</a>
         </li>
         <li>
-          <a href="./electronics">Electronics</a>
+          <a href="#/electronics">Electronics</a>
         </li>
         <li>
-          <a href="./furnitures">Furnitures</a>
+          <a href="#/furnitures">Furnitures</a>
         </li>
         <li>
-          <a href="./toys">Toys</a>
+          <a href="#/toys">Toys</a>
         </li>
         <li>
-          <a href="./others">Others</a>
+          <a href="#/others">Others</a>
         </li>
       </ul>
       <ul>
         <li>
-          <a href="./orders">My orders</a>
+          <a href="#/orders">My orders</a>
         </li>
         <li>
-          <a href="./account">My account</a>
+          <a href="#/account">My account</a>
         </li>
       </ul>
       <ul>
