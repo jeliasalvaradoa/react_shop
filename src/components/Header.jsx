021baebc414327a66ce8d6c139a39d0f77c526d2
jeliasalvaradoa <josealvarado30@hotmail.com>
2023-02-07 01:30:04 -0400

    actualización

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,16 +33,16 @@ const Header = () => {
                         <a href="./">All</a>
                     </li>
                     <li>
-                        <a href="./clothes">Clothes</a>
+                        <a href="/clothes">Clothes</a>
                     </li>
                     <li>
                         <a href="./electronics">Electronics</a>
                     </li>
                     <li>
-                        <a href="./furnitures">Furnitures</a>
+                        <a href="/furnitures">Furnitures</a>
                     </li>
                     <li>
-                        <a href="./toys">Toys</a>
+                        <a href="/toys">Toys</a>
                     </li>
                     <li>
                         <a href="./others">Others</a>
